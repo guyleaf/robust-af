5e345135f13eab63e87a0c05d1817432073b8946
Leaf Ying <leaf.ying.work@gmail.com>
2025-06-05 14:30:13 +0800

    Add an installation script and codna env file for detrex

---
+++ scripts/setup_detrex.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+envName="detrex"
+detrexPath="3rdparty/detrex"
+
+# create conda env
+conda env create -y -f detrex.yml -n "$envName"
+eval "$(conda shell.bash hook)"
+conda activate "$envName"
+
+# install detectron2
+pip install -e "$detrexPath/detectron2"
+
+# install detrex
+pip install -e "$detrexPath"
