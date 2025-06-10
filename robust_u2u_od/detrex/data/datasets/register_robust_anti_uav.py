575b55e516ef5d3b5f8007c97e8145cde80ee580
Leaf Ying <leaf.ying.work@gmail.com>
2025-06-10 20:49:29 +0800

    Add some tags as metadata to Robust Anti-UAV register for better logging

--- robust_u2u_od/detrex/data/datasets/register_robust_anti_uav.py
+++ robust_u2u_od/detrex/data/datasets/register_robust_anti_uav.py
@@ -7,14 +7,24 @@ IMAGES_ROOT = "harmonized"
 TRAIN_ANN_FILE = os.path.join("annotations", "train.json")
 VAL_ANN_FILE = os.path.join("annotations", "val.json")
 
+METADATA = dict(
+    tags=[
+        "Robust Anti-UAV",
+        "Harmonized",
+        "Image2Weather",
+        "Clear",
+        "Cloudy",
+    ]
+)
+
 
 def register_robust_anti_uav(root: str, name: str):
     images_root = os.path.join(root, IMAGES_ROOT)
     train_ann_file = os.path.join(root, TRAIN_ANN_FILE)
     val_ann_file = os.path.join(root, VAL_ANN_FILE)
 
-    register_coco_instances(f"{name}_train", {}, train_ann_file, images_root)
-    register_coco_instances(f"{name}_val", {}, val_ann_file, images_root)
+    register_coco_instances(f"{name}_train", METADATA, train_ann_file, images_root)
+    register_coco_instances(f"{name}_val", METADATA, val_ann_file, images_root)
 
 
 _root = os.path.expanduser(os.getenv("DETECTRON2_DATASETS", "datasets"))
