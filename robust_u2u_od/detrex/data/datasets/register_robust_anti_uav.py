247262e4292bb0c3de812a84622b89146a5cb995
Leaf Ying <leaf.ying.work@gmail.com>
2025-06-11 15:00:56 +0800

    Get the number of images for metadata

--- robust_u2u_od/detrex/data/datasets/register_robust_anti_uav.py
+++ robust_u2u_od/detrex/data/datasets/register_robust_anti_uav.py
@@ -1,7 +1,9 @@
+import json
 import os
 
 from detectron2.data import MetadataCatalog
 from detectron2.data.datasets import register_coco_instances
+from detectron2.utils.file_io import PathManager
 
 DATASET_NAME = "robust_anti_uav"
 IMAGES_ROOT = "harmonized"
@@ -19,13 +21,27 @@ METADATA = dict(
 )
 
 
+def _get_number_images(json_file: str):
+    json_file = PathManager.get_local_path(json_file)
+    with open(json_file, "r") as f:
+        content = json.load(f)
+        return len(content["images"])
+
+
 def register_robust_anti_uav(root: str, name: str):
     images_root = os.path.join(root, IMAGES_ROOT)
     train_ann_file = os.path.join(root, TRAIN_ANN_FILE)
     val_ann_file = os.path.join(root, VAL_ANN_FILE)
 
-    register_coco_instances(f"{name}_train", {}, train_ann_file, images_root)
-    register_coco_instances(f"{name}_val", {}, val_ann_file, images_root)
+    num_train_images = _get_number_images(train_ann_file)
+    num_val_images = _get_number_images(val_ann_file)
+
+    register_coco_instances(
+        f"{name}_train", dict(num_images=num_train_images), train_ann_file, images_root
+    )
+    register_coco_instances(
+        f"{name}_val", dict(num_images=num_val_images), val_ann_file, images_root
+    )
     # one metadata for all subsets
     MetadataCatalog.get(name).set(**METADATA)
 
