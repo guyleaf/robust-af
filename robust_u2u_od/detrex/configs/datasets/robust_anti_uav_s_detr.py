48b029648db0fb88f802d60f0ce9098a51da77f3
Leaf Ying <leaf.ying.work@gmail.com>
2025-06-11 17:12:05 +0800

    Add missing dataloader to robust_anti_uav_s_detr.py

--- robust_u2u_od/detrex/configs/datasets/robust_anti_uav_s_detr.py
+++ robust_u2u_od/detrex/configs/datasets/robust_anti_uav_s_detr.py
@@ -2,6 +2,8 @@ from detectron2.data import MetadataCatalog
 
 from robust_u2u_od.detrex.data.datasets.register_robust_anti_uav import DATASET_NAME
 
+from .robust_anti_uav_detr import dataloader  # noqa: F401
+
 MetadataCatalog.get(DATASET_NAME).tags += [
     "Small",
     "Low-Poly",
