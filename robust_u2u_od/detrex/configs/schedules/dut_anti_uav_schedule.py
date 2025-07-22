0254e59acdcb91592350586dc383c83fa6658f9f
Leaf Ying <leaf.ying.work@gmail.com>
2025-07-22 21:41:02 +0800

    Import module via full path in configs

--- robust_u2u_od/detrex/configs/schedules/dut_anti_uav_schedule.py
+++ robust_u2u_od/detrex/configs/schedules/dut_anti_uav_schedule.py
@@ -1,4 +1,5 @@
-from ...data.datasets.register_dut_anti_uav import DATASET_NAME
+from robust_u2u_od.detrex.data.datasets.register_dut_anti_uav import DATASET_NAME
+
 from .common_schedule import default_detr_schedulers
 
 detr_schedulers = default_detr_schedulers(f"{DATASET_NAME}_train")
