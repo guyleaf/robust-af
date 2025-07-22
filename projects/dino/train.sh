b3da5e034d19b37632277d992846c657835bddb3
Leaf Ying <leaf.ying.work@gmail.com>
2025-07-22 17:09:01 +0800

    Optimize the training scripts in project DINO

--- projects/dino/train.sh
+++ projects/dino/train.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
+set -eu
 
 CWD=$(dirname "$0")
-ROOT=$(dirname "$(dirname "$CWD")")
+DETREX_ROOT="$(dirname "$(dirname "$CWD")")/3rdparty/detrex"
 
 CONFIG=$1
 GPUS=${GPUS:-4}
+PORT=${PORT:-25001}
 
-python "$ROOT/tools/train_net.py" \
+python "$DETREX_ROOT/tools/train_net.py" \
     --config-file "$CONFIG" \
     --num-gpus "$GPUS" \
+    --dist-url "tcp://127.0.0.1:$PORT" \
     "${@:2}"
