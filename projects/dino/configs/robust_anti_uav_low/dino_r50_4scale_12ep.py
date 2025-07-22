3450637c6091cc511a0f3002abd39b94584a19e1
Leaf Ying <leaf.ying.work@gmail.com>
2025-07-22 17:10:54 +0800

    Save current configs

--- projects/dino/configs/robust_anti_uav_low/dino_r50_4scale_12ep.py
+++ projects/dino/configs/robust_anti_uav_low/dino_r50_4scale_12ep.py
@@ -8,9 +8,6 @@ from robust_u2u_od.detrex.data.datasets.register_robust_anti_uav import DATASET_
 
 from ..models.dino_r50 import model
 
-metadata = MetadataCatalog.get(DATASET_NAME)
-train_metadata = MetadataCatalog.get(f"{DATASET_NAME}_train")
-
 # get default config
 dataloader = get_config("datasets/robust_anti_uav_low_detr.py").dataloader
 optimizer = get_upstream_config("common/optim.py").AdamW
@@ -18,6 +15,9 @@ lr_multiplier = get_config(
     "schedules/robust_anti_uav_schedule.py"
 ).detr_schedulers.lr_multiplier_12ep_8bs
 train = get_upstream_config("common/train.py").train
+
+metadata = MetadataCatalog.get(DATASET_NAME)
+train_metadata = MetadataCatalog.get(f"{DATASET_NAME}_train")
 
 # ==============================================================
 
@@ -32,8 +32,9 @@ total_batch_size = 8
 lr = base_lr * (total_batch_size / base_batch_size)
 
 num_epochs = 12
-output_dir = os.path.expanduser(
-    f"~/work/work_dirs/detrex/dino_r50_4scale/{DATASET_NAME}_s_low/dino_r50_4scale_12ep"
+output_dir = os.path.expanduser(os.getenv("R_U2U_OD_WORKDIR", "./outputs"))
+output_dir = os.path.join(
+    output_dir, f"dino_r50_4scale/{DATASET_NAME}_low/dino_r50_4scale_12ep"
 )
 
 # wandb settings
@@ -51,9 +52,11 @@ num_images: int = train_metadata.num_images
 # because drop_last=True
 num_batches = num_images // total_batch_size
 train.max_iter = num_epochs * num_batches
+# eval per epoch
 train.eval_period = num_batches
 train.log_period = 20
 train.checkpointer.period = num_batches
+train.checkpointer.max_to_keep = 3
 
 # gradient clipping for training
 train.clip_grad.enabled = True
@@ -61,7 +64,7 @@ train.clip_grad.params.max_norm = 0.1
 train.clip_grad.params.norm_type = 2
 
 # change the number of classes
-model.num_classes = train_metadata.thing_classes
+model.num_classes = metadata.num_classes
 
 # set training devices
 train.device = "cuda"
@@ -75,6 +78,9 @@ optimizer.params.lr_factor_func = (
     lambda module_name: 0.1 if "backbone" in module_name else 1
 )
 
+# modify dataloader config
+dataloader.train.num_workers = 4
+
 # please notice that this is total batch size.
 # surpose you're using 4 gpus for training and the batch size for
 # each gpu is 16/4 = 4
@@ -87,7 +93,7 @@ dataloader.evaluator.output_dir = train.output_dir
 train.wandb = dict(
     enabled=True,
     params=dict(
-        dir=os.path.join(output_dir, "wandb"),
+        dir=output_dir,
         project="robust-u2u-od",
         group="dino_r50_4scale_12ep",
         job_type="from scratch",
@@ -95,3 +101,6 @@ train.wandb = dict(
         notes=notes,
     ),
 )
+
+# set the random seed
+train.seed = 2025
