8a42ccc717ea4967e5ae7c30ba96709e3f165589
Leaf Ying <leaf.ying.work@gmail.com>
2025-07-31 22:10:37 +0800

    Support fast_dev_run mode in DINO project

--- projects/dino/train_net.py
+++ projects/dino/train_net.py
@@ -283,6 +283,12 @@ def main(args):
     cfg = LazyConfig.load(args.config_file)
     cfg = LazyConfig.apply_overrides(cfg, args.opts)
     default_setup(cfg, args)
+    
+    # Enable fast debugging by running several iterations to check for any bugs.
+    if cfg.train.fast_dev_run.enabled:
+        cfg.train.max_iter = 20
+        cfg.train.eval_period = 10
+        cfg.train.log_period = 1
 
     if args.eval_only:
         model = instantiate(cfg.model)
