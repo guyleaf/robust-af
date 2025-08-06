2742abef18ac75abb8ef4b149716b7863e9247ff
Leaf Ying <leaf.ying.work@gmail.com>
2025-08-06 15:51:57 +0800

    Remove editable installation from yolov10 repo due to reportMissingImports reported by pylance

--- scripts/setup_yolov10.sh
+++ scripts/setup_yolov10.sh
@@ -13,4 +13,6 @@ conda activate "$envName"
 pip install -r "$sourcePath/requirements.txt"
 
 # install yolov10
-pip install -e "$sourcePath"
+pip install "$sourcePath"
+
+echo -e "\033[0;32mInstallation Successfully!"
