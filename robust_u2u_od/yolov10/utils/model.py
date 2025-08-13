d65108af51bfbb7dc6180dca951df1e7ec2d613e
Leaf Ying <leaf.ying.work@gmail.com>
2025-08-13 14:09:01 +0800

    Add a helper function for checking if it is a repo of huggingface hub

---
+++ robust_u2u_od/yolov10/utils/model.py
@@ -0,0 +1,11 @@
+from huggingface_hub import repo_exists
+from huggingface_hub.errors import HFValidationError
+from huggingface_hub.utils import validate_repo_id
+
+
+def is_huggingface_hub_model(model: str):
+    try:
+        validate_repo_id(model)
+        return repo_exists(model)
+    except HFValidationError:
+        return False
