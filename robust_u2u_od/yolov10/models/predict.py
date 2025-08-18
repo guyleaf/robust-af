2041f3eb6e9d8fa303c1e31ae86b4bcb27e47c9f
Leaf Ying <leaf.ying.work@gmail.com>
2025-08-18 21:04:06 +0800

    Finished integrating robust module and datasets into YOLOv10

--- robust_u2u_od/yolov10/models/predict.py
+++ robust_u2u_od/yolov10/models/predict.py
@@ -2,9 +2,14 @@ from ultralytics.models.yolov10 import (
     YOLOv10DetectionPredictor as ORIGINAL_YOLOv10DetectionPredictor,
 )
 
-from ..utils import DEFAULT_CFG
+from ..utils import DEFAULT_CFG, DEFAULT_ROBUST_CFG
 
 
 class YOLOv10DetectionPredictor(ORIGINAL_YOLOv10DetectionPredictor):
     def __init__(self, cfg=DEFAULT_CFG, overrides=None, _callbacks=None):
+        super().__init__(cfg, overrides, _callbacks)
+
+
+class RobustYOLOv10DetectionPredictor(YOLOv10DetectionPredictor):
+    def __init__(self, cfg=DEFAULT_ROBUST_CFG, overrides=None, _callbacks=None):
         super().__init__(cfg, overrides, _callbacks)
