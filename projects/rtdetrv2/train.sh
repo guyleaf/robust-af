34570ef394eca07307cc86d80d1878749bb86fdf
Leaf Ying <leaf.ying.work@gmail.com>
2025-08-28 20:56:55 +0800

    Add train and test scripts for rtdetrv2

---
+++ projects/rtdetrv2/train.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -eu
+
+CWD=$(dirname "$0")
+
+CONFIG=$1
+GPUS=${GPUS:-4}
+PORT=${PORT:-9909}
+SEED=${SEED:-2025}
+
+torchrun \
+    --master-port "$PORT" \
+    --nproc-per-node "$GPUS" \
+    "$CWD/tools/train.py" \
+    --config "$CONFIG" \
+    --print-method rich \
+    --seed "$SEED" \
+    "${@:2}"
