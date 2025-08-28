b47571617cfbaaca14fd975d32311bbdb6b42253
Leaf Ying <leaf.ying.work@gmail.com>
2025-08-28 21:13:05 +0800

    Update the defaults of YOLOv10 config

--- robust_u2u_od/yolov10/utils/cfg.py
+++ robust_u2u_od/yolov10/utils/cfg.py
@@ -9,11 +9,11 @@ from ultralytics.utils import (
 # Default configuration
 DEFAULT_CFG_DICT = dict(
     **DEFAULT_CFG_DICT,
-    degradation=dict(enabled=False, seed=None, identity=True, ignored_degradations=[]),
+    degradation=dict(enabled=False, seed=2025, identity=True, ignored_degradations=[]),
 )
 DEFAULT_ROBUST_CFG_DICT = dict(
     **DEFAULT_CFG_DICT,
-    cst_loss=dict(module="nn.MSELoss", weight=20),
+    cst_loss=dict(module="nn.MSELoss", weight=10),
     eval_bn_on_freeze=True,
 )
 DEFAULT_CFG = IterableSimpleNamespace(**DEFAULT_CFG_DICT)
