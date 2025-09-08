3649a3c894f0d836afc40161945973fd1666b270
Leaf Ying <leaf.ying.work@gmail.com>
2025-09-08 22:19:32 +0800

    Implement FrozenBatchNorm variants to freeze batch norms

--- robust_u2u_od/models/__init__.py
+++ robust_u2u_od/models/__init__.py
@@ -1,4 +1,9 @@
 # ruff: noqa: F401
 from . import robust_layers
+from .frozen_batch_norm import (
+    FrozenBatchNorm2d,
+    FrozenBatchNormConverterMixin,
+    FrozenSyncBatchNorm,
+)
 
 __all__ = list(globals().keys())
