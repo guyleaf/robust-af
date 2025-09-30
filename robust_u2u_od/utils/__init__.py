ac77f3164395503e7e086c0899d33a43f8fb708a
Leaf Ying <leaf.ying.work@gmail.com>
2025-09-30 14:11:11 +0800

    feat(utils): Support finding and importing from 3rdparty submodules

--- robust_u2u_od/utils/__init__.py
+++ robust_u2u_od/utils/__init__.py
@@ -1,7 +1,7 @@
 # ruff: noqa: F401
 from .dataset import format_coco_image
 from .dist import get_worker_id
-from .misc import format_size
+from .misc import add_3rdparty_submodule, find_3rdparty_submodule, format_size
 from .model import (
     convert_to_batchnorm_2d,
     convert_to_frozen_batchnorm_2d,
--- robust_u2u_od/utils/misc.py
+++ robust_u2u_od/utils/misc.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+
+THIRD_PARTY_ROOT = Path(__file__).resolve().parents[2] / "3rdparty"
+
+
 # copied from https://github.com/open-mmlab/mmengine/blob/main/mmengine/analysis/print_helper.py#L20
 def format_size(x: int, sig_figs: int = 3, hide_zero: bool = False) -> str:
     """Formats an integer for printing in a table or model representation.
@@ -33,3 +39,32 @@ def format_size(x: int, sig_figs: int = 3, hide_zero: bool = False) -> str:
     if abs(x) > 1e2:
         return fmt(x / 1e3) + "K"
     return str(x)
+
+
+def find_3rdparty_submodule(name: str):
+    """Find the 3rd-party submodule by name
+
+    Args:
+        name (str): Name of the 3rd-party submodule
+
+    Raises:
+        ValueError: The 3rd-party submodule is not found.
+
+    Returns:
+        pathlib.Path: The root of the 3rd-party submodule
+    """
+    path = THIRD_PARTY_ROOT / name
+    if not path.is_dir():
+        raise ValueError(f"The submodule is not found in {path}.")
+    return path
+
+
+def add_3rdparty_submodule(name: str):
+    """Prepend 3rd-party submodule to sys.path
+
+    Args:
+        name (str): Name of the 3rd-party submodule
+    """
+    path = str(find_3rdparty_submodule(name))
+    if path not in sys.path:
+        sys.path.insert(0, path)
