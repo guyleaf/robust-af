bfb1a806acbb2458ff499288000279a3ba2945aa
Leaf Ying <leaf.ying.work@gmail.com>
2025-10-03 19:00:37 +0800

    fix(rtdetrv2): Fix potential incorrect random order of data augmentations

--- robust_u2u_od/rtdetrv2/data/dataset/coco_dataset.py
+++ robust_u2u_od/rtdetrv2/data/dataset/coco_dataset.py
@@ -6,7 +6,6 @@ import torch
 import torch.nn as nn
 from rtdetrv2.core import register
 from rtdetrv2.data import CocoDetection as ORIGINAL_CocoDetection
-from rtdetrv2.data import Compose, EmptyTransform
 
 from ....utils import RandomContext
 
@@ -27,14 +26,17 @@ class RobustCocoDetection(ORIGINAL_CocoDetection):
         super().__init__(
             img_folder, ann_file, transforms, return_masks, remap_mscoco_category
         )
-
-        if self._transforms is None:
-            self._transforms = EmptyTransform()
-        self._robust_transforms = Compose([robust_transforms, self._transforms])
+        self._robust_transforms = robust_transforms
 
     def __getitem__(self, idx: int):
         img, target = self.load_item(idx)
-        with RandomContext():
-            clear_img, _, _ = self._transforms(img.copy(), deepcopy(target), self)
+        clear_img, clear_target = img.copy(), deepcopy(target)
+
         img, target, _ = self._robust_transforms(img, target, self)
+        if self._transforms is not None:
+            with RandomContext():
+                clear_img, clear_target, _ = self._transforms(
+                    clear_img, clear_target, self
+                )
+            img, target, _ = self._transforms(img, target, self)
         return torch.stack([img, clear_img]), target
