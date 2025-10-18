1352c7c38896864ea3119370a3c5ad95df667494
Leaf Ying <leaf.ying.work@gmail.com>
2025-10-18 18:30:03 +0800

    chore(detrex): Register our custom classes for detrex before loading detectron2 via DETECTRON2_ENV_MODULE env variable

--- robust_u2u_od/detrex/__init__.py
+++ robust_u2u_od/detrex/__init__.py
@@ -2,3 +2,8 @@
 from . import configs, data, engine, modeling, utils
 
 __all__ = list(globals().keys())
+
+
+def setup_environment():
+    # Dummy setup function for preloading before detectron2 via DETECTRON2_ENV_MODULE env variable
+    pass
