cda92607069cc4a8971233a84171ac3a636ac70d
Leaf Ying <leaf.ying.work@gmail.com>
2025-10-19 19:32:22 +0800

    refactor(yolov10): Change to use shared FrozenBatchNorm converter and Support int and list for freeze_bn setting

--- robust_u2u_od/yolov10/models/train.py
+++ robust_u2u_od/yolov10/models/train.py
@@ -9,9 +9,7 @@ from ultralytics.utils import LOGGER, RANK
 from ultralytics.utils.plotting import plot_images
 from ultralytics.utils.torch_utils import de_parallel
 
-from robust_u2u_od.utils import ReproducibleRandomContext
-from robust_u2u_od.yolov10.utils import update_batch_norm_mode
-
+from ...utils import ReproducibleRandomContext, convert_to_frozen_batchnorm_2d
 from ..data import build_yolo_dataset
 from ..nn.tasks import RobustYOLOv10DetectionModel
 from ..utils import (
@@ -133,27 +131,6 @@ class RobustYOLOv10DetectionTrainer(YOLOv10DetectionTrainer):
 
     def get_model(self, cfg=None, weights=None, verbose=True):
         """Return a YOLO detection model."""
-        if self.args.eval_bn_on_freeze:
-            # wrap the train method to set BatchNorm2d to eval mode if frozon
-            freeze_list = (
-                self.args.freeze
-                if isinstance(self.args.freeze, list)
-                else list(range(self.args.freeze))
-                if isinstance(self.args.freeze, int)
-                else []
-            )
-
-            def _train(self: RobustYOLOv10DetectionModel, mode: bool = True):
-                super(RobustYOLOv10DetectionModel, self).train(mode)
-                if mode:
-                    for i in freeze_list:
-                        update_batch_norm_mode(self.model[i], False)
-                    if verbose:
-                        LOGGER.info("Set BatchNorms in freeze list to eval mode!")
-                return self
-
-            RobustYOLOv10DetectionModel.train = _train
-
         model = RobustYOLOv10DetectionModel(
             cfg,
             nc=self.data["nc"],
@@ -161,6 +138,30 @@ class RobustYOLOv10DetectionTrainer(YOLOv10DetectionTrainer):
         )
         if weights:
             model.load(weights)
+
+        if self.args.freeze_bn:
+            if isinstance(self.args.freeze_bn, bool):
+                freeze_list = self.args.freeze
+            elif isinstance(self.args.freeze_bn, (int, list)):
+                freeze_list = self.args.freeze_bn
+            else:
+                raise ValueError(
+                    "The freeze_bn argument only supports boolean, int or list."
+                )
+
+            freeze_list = (
+                freeze_list
+                if isinstance(freeze_list, list)
+                else list(range(freeze_list))
+                if isinstance(freeze_list, int)
+                else []
+            )
+            for i in freeze_list:
+                # NOTE: currently, only supports freezing BatchNorm2d
+                model.model[i] = convert_to_frozen_batchnorm_2d(model.model[i])
+
+            if verbose:
+                LOGGER.info("Freeze BatchNorms!")
         return model
 
     def plot_training_samples(self, batch: dict, ni: int):
