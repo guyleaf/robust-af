56ef879531f8951380879cc73240c1fd83848856
Leaf Ying <leaf.ying.work@gmail.com>
2025-10-22 15:25:46 +0800

    chore: Optimize setup scripts for rtdetrv2 and detrex

--- scripts/setup_detrex.sh
+++ scripts/setup_detrex.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
+cwd=$(dirname "$0")
+root=$(dirname "$cwd")
+
 envName="detrex"
-detrexPath="3rdparty/detrex"
+sourcePath="$root/3rdparty/detrex"
 
 # create conda env
-conda env create -y -f detrex.yml -n "$envName"
+conda env create -y -f "$root/detrex.yml" -n "$envName"
 eval "$(conda shell.bash hook)"
 conda activate "$envName"
 
 # install detectron2
-pip install -e "$detrexPath/detectron2"
+pip install -e "$sourcePath/detectron2"
 
 # install detrex
-pip install -e "$detrexPath"
+pip install -e "$sourcePath"
 
 echo -e "\033[0;32mInstallation Successfully!"
--- scripts/setup_rtdetrv2.sh
+++ scripts/setup_rtdetrv2.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
+cwd=$(dirname "$0")
+root=$(dirname "$cwd")
+
 envName="rtdetrv2"
-rtdetrv2Path="3rdparty/RT-DETR/rtdetrv2_pytorch"
+sourcePath="$root/3rdparty/RT-DETR/rtdetrv2_pytorch"
 
 # create conda env
-conda env create -y -f rtdetrv2.yml -n "$envName"
+conda env create -y -f "$root/rtdetrv2.yml" -n "$envName"
 eval "$(conda shell.bash hook)"
 conda activate "$envName"
 
 # install rtdetrv2
-pip install -e "$rtdetrv2Path"
+pip install -e "$sourcePath"
+
+echo -e "\033[0;32mInstallation Successfully!"
