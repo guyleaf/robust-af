c4276c66053c7fc78034a13bfe09fe280c109259
Leaf Ying <leaf.ying.work@gmail.com>
2025-10-22 10:39:18 +0800

    chore(yolov10): Make dataset, weight, experiment folder structure consistent with other base models

--- scripts/setup_yolov10.sh
+++ scripts/setup_yolov10.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+cwd=$(dirname "$0")
+root=$(dirname "$cwd")
+
 envName="yolov10"
-sourcePath="3rdparty/yolov10"
+sourcePath="$root/3rdparty/yolov10"
+projectPath="$root/projects/yolov10"
 
 # create conda env
-conda env create -y -f yolov10.yml -n "$envName"
+conda env create -y -f "$root/yolov10.yml" -n "$envName"
 eval "$(conda shell.bash hook)"
 conda activate "$envName"
 
@@ -14,5 +18,9 @@ pip install -r "$sourcePath/requirements.txt"
 
 # install yolov10
 pip install -e "$sourcePath"
+
+# configure yolo settings
+# NOTE: runs_dir will only work if project setting is not specified! Please check the project/yolov10/README.md
+yolo settings datasets_dir="$projectPath/datasets" weights_dir="$projectPath/weights" runs_dir="$projectPath/runs"
 
 echo -e "\033[0;32mInstallation Successfully!"
