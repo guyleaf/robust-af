e5c80c9c816be65fe1812a2f5c09f73abcb14fcf
Leaf Ying <leaf.ying.work@gmail.com>
2025-11-14 16:58:23 +0800

    feat(detrex): Support test subset of DUT Anti-UAV

--- robust_au_od/detrex/data/datasets/register_dut_anti_uav.py
+++ robust_au_od/detrex/data/datasets/register_dut_anti_uav.py
@@ -8,6 +8,7 @@ DATASET_NAME = DATASET_FOLDER.lower()
 IMAGES_ROOT = "images"
 TRAIN_ANN_FILE = os.path.join("annotations", "train.json")
 VAL_ANN_FILE = os.path.join("annotations", "val.json")
+TEST_ANN_FILE = os.path.join("annotations", "test.json")
 
 METADATA = dict(
     num_classes=1,
@@ -21,13 +22,15 @@ def register_dut_anti_uav(root: str, name: str):
     images_root = os.path.join(root, IMAGES_ROOT)
     train_ann_file = os.path.join(root, TRAIN_ANN_FILE)
     val_ann_file = os.path.join(root, VAL_ANN_FILE)
+    test_ann_file = os.path.join(root, TEST_ANN_FILE)
 
     register_coco_instances(f"{name}_train", {}, train_ann_file, images_root)
     register_coco_instances(f"{name}_val", {}, val_ann_file, images_root)
+    register_coco_instances(f"{name}_test", {}, test_ann_file, images_root)
     # one metadata for all subsets
     MetadataCatalog.get(name).set(**METADATA)
 
 
 _root = os.path.expanduser(os.getenv("DETECTRON2_DATASETS", "datasets"))
-_root = os.path.join(_root, DATASET_FOLDER)
+_root = os.path.join(_root, DATASET_FOLDER, "detection")
 register_dut_anti_uav(_root, DATASET_NAME)
