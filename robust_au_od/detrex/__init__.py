dcc7065599512cf500caeb99b5649c406529985b
Leaf Ying <leaf.ying.work@gmail.com>
2025-11-26 13:06:02 +0800

    fix(detrex): Move setup_environment to the top to avoid import missing during initialization

--- robust_au_od/detrex/__init__.py
+++ robust_au_od/detrex/__init__.py
@@ -1,9 +1,9 @@
-# ruff: noqa: F401
-from . import configs, data, engine, modeling, utils
-
-__all__ = list(globals().keys())
-
-
+# ruff: noqa: F401, E402
 def setup_environment():
     # Dummy setup function for preloading before detectron2 via DETECTRON2_ENV_MODULE env variable
     pass
+
+
+from . import configs, data, engine, modeling, utils
+
+__all__ = list(globals().keys())
