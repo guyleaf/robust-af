9e201808bceb076d7d68488a2bdfab8fc89f3e90
Leaf Ying <leaf.ying.work@gmail.com>
2025-12-26 13:09:02 +0800

    fix(rtdetrv2): Move setup_environment() before importing other modules to avoid module not found

--- robust_au_od/rtdetrv2/__init__.py
+++ robust_au_od/rtdetrv2/__init__.py
@@ -1,9 +1,11 @@
-# ruff: noqa: F401
-from . import data, nn, solver, zoo
-
-__all__ = list(globals().keys())
-
-
+# ruff: noqa: F401, E402
 def setup_environment():
     """setup function for preloading before rtdetrv2 via RTDETRV2_ENV_MODULE env variable"""
+    from . import solver
+
     solver.setup_environment()
+
+
+from . import data, nn, zoo
+
+__all__ = list(globals().keys())
