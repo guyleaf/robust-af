340ec4596adc8ec99bd536a06bbb6a10177db8a4
Leaf Ying <leaf.ying.work@gmail.com>
2026-01-07 12:59:53 +0800

    feat: Support sampling a degradation and applying the specific degradation

--- robust_au_od/transforms/__init__.py
+++ robust_au_od/transforms/__init__.py
@@ -1,4 +1,9 @@
 # ruff: noqa: F401
-from .degradations import DEGRADATION_TRANSFORMS, apply_degradation
+from .degradations import (
+    DEGRADATION_TRANSFORMS,
+    apply_degradation,
+    apply_random_degradation,
+    sample_degradation_name,
+)
 
 __all__ = list(globals().keys())
