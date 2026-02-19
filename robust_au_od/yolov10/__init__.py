069455f4d17123f2ae0ffa38445a3d6f63a865ad
Leaf Ying <leaf.ying.work@gmail.com>
2026-02-19 23:57:10 +0800

    feat(yolov10): Support customizing keys in data config to resolve paths

---
+++ robust_au_od/yolov10/__init__.py
@@ -0,0 +1,12 @@
+# ruff: noqa: F401, E402
+def setup_environment():
+    """setup function for preloading before ultralytics via ULTRALYTICS_ENV_MODULE env variable"""
+    from functools import partial
+
+    import ultralytics.data.utils as data_utils
+
+    # NOTE: hacky way, configure keys which will be resolved by path
+    data_utils.check_det_dataset = partial(
+        data_utils.check_det_dataset,
+        resolve_paths=("train", "val", "test", "degraded_val", "degraded_test"),
+    )
