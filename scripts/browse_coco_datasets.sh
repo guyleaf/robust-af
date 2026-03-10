046e2e6a2caf46ffd4bf1623c8db054bc2442707
Leaf Ying <leaf.ying.work@gmail.com>
2026-03-10 22:40:06 +0800

    chore: Add a script for batch visualizing datasets

---
+++ scripts/browse_coco_datasets.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+cwd=$(dirname "$0")
+root=$(dirname "$cwd")
+out_dir=${1:-$root/images/vis}
+
+python "$cwd/browse_coco_dataset.py" --out-dir "$out_dir/dut_anti_uav" ~/data/UAV/DUT_Anti_UAV/detection
+python "$cwd/browse_coco_dataset.py" --out-dir "$out_dir/dut_anti_uav" ~/data/UAV/DUT_Anti_UAV/tracking
+python "$cwd/browse_coco_dataset.py" --out-dir "$out_dir" ~/data/UAV/Det_Fly
+python "$cwd/browse_coco_dataset.py" --out-dir "$out_dir" ~/data/UAV/MAV_VID
+python "$cwd/browse_coco_dataset.py" --out-dir "$out_dir" ~/data/UAV/DDS
+python "$cwd/browse_coco_dataset.py" --out-dir "$out_dir" ~/data/UAV/Sim2Air/UAV-Eagle
+python "$cwd/browse_coco_dataset.py" --out-dir "$out_dir" ~/data/UAV/Sim2Air/S-UAV-T
