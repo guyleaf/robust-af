a2247f15bacd027cc2aa60065934215299478a4c
Leaf Ying <leaf.ying.work@gmail.com>
2026-03-12 13:34:30 +0800

    refactor: Rename UAV-Eagle to UAV_Eagle to match the naming convention

--- scripts/browse_coco_datasets.sh
+++ scripts/browse_coco_datasets.sh
@@ -10,5 +10,5 @@ python "$cwd/browse_coco_dataset.py" --out-dir "$out_dir/dut_anti_uav" ~/data/UA
 python "$cwd/browse_coco_dataset.py" --out-dir "$out_dir" ~/data/UAV/Det_Fly
 python "$cwd/browse_coco_dataset.py" --out-dir "$out_dir" ~/data/UAV/MAV_VID
 python "$cwd/browse_coco_dataset.py" --out-dir "$out_dir" ~/data/UAV/DDS
-python "$cwd/browse_coco_dataset.py" --out-dir "$out_dir" ~/data/UAV/Sim2Air/UAV-Eagle
-python "$cwd/browse_coco_dataset.py" --out-dir "$out_dir" ~/data/UAV/Sim2Air/S-UAV-T
+python "$cwd/browse_coco_dataset.py" --out-dir "$out_dir" ~/data/UAV/Sim2Air/UAV_Eagle
+python "$cwd/browse_coco_dataset.py" --out-dir "$out_dir" ~/data/UAV/Sim2Air/S_UAV_T
