1c5ac64270faf64fd860ea14d375fd05306adc90
Leaf Ying <leaf.ying.work@gmail.com>
2026-03-20 15:08:15 +0800

    chore: Make a dummy submodule for feature-level baselines

--- robust_af/models/feature_adapters/__init__.py
+++ robust_af/models/feature_adapters/__init__.py
@@ -1,4 +1,6 @@
 # ruff: noqa: F401
+# don't import baselines here. Please keep it as an extra functionality.
+
 from .afr import AFR, FrequencyAFR, SpatialAFR
 from .amfg import AMFG, FrequencyAMFG, SpatialAMFG
 from .amfg_v2 import AMFGv2, FrequencyAMFGv2, SpatialAMFGv2
