c2a9ebedb44751a6788d02873ae58ff80c85010c
Leaf Ying <leaf.ying.work@gmail.com>
2026-04-18 16:03:18 +0800

    feat(yolov10): Support saving config during eval

--- robust_af/yolov10/models/val.py
+++ robust_af/yolov10/models/val.py
@@ -3,7 +3,7 @@ import json
 from ultralytics.models.yolov10 import (
     YOLOv10DetectionValidator as ORIGINAL_YOLOv10DetectionValidator,
 )
-from ultralytics.utils import LOGGER, RANK
+from ultralytics.utils import LOGGER, yaml_save
 from ultralytics.utils.plotting import plot_images
 from ultralytics.utils.torch_utils import init_seeds
 
@@ -14,11 +14,14 @@ from ..utils import DEFAULT_CFG, DEFAULT_ROBUST_CFG
 class YOLOv10DetectionValidator(ORIGINAL_YOLOv10DetectionValidator):
     def __init__(self, *args, cfg=DEFAULT_CFG, **kwargs):
         super().__init__(*args, cfg=cfg, **kwargs)
+        if self.args.mode == "val":
+            self.args.save_dir = str(self.save_dir)
+            yaml_save(self.save_dir / "args.yaml", vars(self.args))  # save run args
 
     def __call__(self, trainer=None, model=None):
         if trainer is None:
             # avoid any randomness during inference (e.g., GDIP)
-            init_seeds(self.args.seed + 1 + RANK, deterministic=self.args.deterministic)
+            init_seeds(self.args.seed, deterministic=self.args.deterministic)
         stats = super().__call__(trainer, model)
         if not self.training:
             # save eval results in json for easier checking
