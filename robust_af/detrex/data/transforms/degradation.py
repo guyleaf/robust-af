07ffdbbb9bca129a5420621f0e348c4c8a93d38d
Leaf Ying <leaf.ying.work@gmail.com>
2026-04-25 10:35:35 +0800

    feat(detrex): Support specifying degradations for random sampling

--- robust_af/detrex/data/transforms/degradation.py
+++ robust_af/detrex/data/transforms/degradation.py
@@ -1,6 +1,6 @@
 import logging
 import random
-from typing import Optional
+from typing import Optional, Union
 
 import numpy as np
 from detectron2.data.transforms import (
@@ -66,17 +66,34 @@ class Degradation(Augmentation):
         name: Optional[str] = None,
         seed: Optional[int] = None,
         identity: bool = True,
-        ignored_degradations: list[str] = [],
+        degradations: Optional[Union[list[str], set[str]]] = None,
+        ignored_degradations: Union[list[str], set[str]] = [],
     ):
+        """
+        Initializes the Degradation class.
+
+        Args:
+            name (str, optional): Apply specific degradation without random sampling from degradations.
+            seed (int, optional): The seed to determine the randomness.
+            identity (bool): Whether include identity (skip connection) in degradations.
+            degradations (array-like, optional): Apply specific degradations. If it is not None, identity option will be ignored.
+            ignored_degradations (array-like): The degradations to be ignored.
+        """
         super().__init__()
         self.name = name
         self.seed = seed
         self.identity = identity
+        self.degradations = degradations
         self.ignored_degradations = ignored_degradations
         self._init_random(None)
 
+        assert not (self.name is not None and self.degradations is not None), (
+            "The name and degradations cannot be set at the same time."
+        )
         if self.name is not None:
             LOGGER.info(f"Specified degradation: {self.name}")
+        elif self.degradations is not None:
+            LOGGER.info(f"Specified degradations: {', '.join(self.degradations)}")
 
     def _init_random(self, seed: Optional[int]):
         if seed is None:
@@ -103,6 +120,7 @@ class Degradation(Augmentation):
         if name is None:
             name = sample_degradation_name(
                 identity=self.identity,
+                transforms=self.degradations,
                 ignored_transforms=self.ignored_degradations,
                 np_random_generator=self.np_random_generator,
                 py_random=self.py_random,
