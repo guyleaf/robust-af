04208097180de897b9740c95cc10b2ad1aed458f
Leaf Ying <leaf.ying.work@gmail.com>
2026-05-02 13:24:27 +0800

    chore: Add a script for copying checkpoints from experiments to dst folder

---
+++ scripts/others/copy_ckpts_to_dst_folder.py
@@ -0,0 +1,35 @@
+import argparse
+import shutil
+from pathlib import Path
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Copy checkpoints to the destination folder"
+    )
+    parser.add_argument("src_dir", help="The source folder")
+    parser.add_argument("dst_dir", help="The destination folder")
+    parser.add_argument(
+        "--pattern",
+        default="*best*.pt?",
+        help="The search pattern for the file (Unix-style wildcards)",
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    src_dir = Path(args.src_dir)
+    dst_dir = Path(args.dst_dir)
+    dst_dir.mkdir(parents=True, exist_ok=True)
+    for exp_folder in src_dir.iterdir():
+        if not exp_folder.is_dir():
+            continue
+
+        files = list(exp_folder.glob(args.pattern))
+        assert len(files) == 1, "Found multiple files in the experiment folder."
+        checkpoint = files[0]
+
+        out_dir = dst_dir / exp_folder.name
+        out_dir.mkdir(exist_ok=True)
+        shutil.copy2(checkpoint, out_dir)
