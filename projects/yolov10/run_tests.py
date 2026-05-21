bac10aacb7d554f1a29c6eb160521bf274f93594
Leaf Ying <leaf.ying.work@gmail.com>
2026-05-21 23:21:52 +0800

    feat(yolov10): Support GPU Pool to distribute workload and specifying degradations in run_tests.py

--- projects/yolov10/run_tests.py
+++ projects/yolov10/run_tests.py
@@ -1,11 +1,14 @@
 import argparse
 import concurrent.futures.thread as thread
 import os
+import queue
 import subprocess
 import sys
 import tempfile
 from concurrent.futures import as_completed
+from dataclasses import dataclass
 from pathlib import Path
+from typing import Optional
 
 import yaml
 from rich.console import Console
@@ -13,8 +16,16 @@ from rich.console import Console
 CONSOLE = Console()
 
 
-def run(config: Path, name: str, split: str, **kwargs):
-    splits = [item for item in split.replace("val", "").split("_") if len(item) != 0]
+@dataclass
+class Dataset:
+    path: Path
+    split: str
+
+
+def run(config: Path, name: str, dataset: Dataset, **kwargs):
+    splits = [
+        item for item in dataset.split.replace("val", "").split("_") if len(item) != 0
+    ]
     splits = list(reversed(splits))
     if len(splits) != 0:
         suffix = "_".join(splits)
@@ -24,9 +35,12 @@ def run(config: Path, name: str, split: str, **kwargs):
         cfg: dict = yaml.safe_load(f)
 
     work_dir = Path(cfg["name"])
-    work_dir = work_dir.with_name(name)
+    # concat with folder of training dataset
+    # test structure: <training dataset>/<testing dataset>/<exp_name>
+    work_dir = work_dir.parent.parent / dataset.path.stem / name
     cfg["name"] = work_dir.as_posix()
-    cfg["split"] = split
+    cfg["data"] = dataset.path.as_posix()
+    cfg["split"] = dataset.split
     cfg.update(**kwargs)
 
     with tempfile.NamedTemporaryFile(
@@ -50,16 +64,40 @@ def run(config: Path, name: str, split: str, **kwargs):
         os.unlink(file.name)
 
 
+def run_with_device(device_pool: queue.Queue, **kwargs):
+    # in theory, it shouldn't be blocked here because the num_workers == num_devices.
+    device: int = device_pool.get_nowait()
+    try:
+        return run(device=device, **kwargs)
+    finally:
+        # always return, even on exception
+        device_pool.put(device)
+
+
 def parse_configs(cfg: dict):
     root = Path(cfg.pop("root"))
+    dataset_root = Path(cfg.pop("dataset_root"))
     prefix: str = cfg.pop("prefix")
+    degradations: Optional[list[str]] = cfg.pop("degradations", None)
     configs: list[dict] = cfg.pop("configs")
+
+    if degradations is None:
+        degradations = ["degraded"]
 
     for config in configs:
         dataset_name = config["name"]
         path = root / f"{prefix}_{dataset_name}.yaml"
-        for split in config["splits"]:
-            yield path, dict(split=split)
+
+        for degradation in degradations:
+            if degradation == "degraded":
+                data = f"{dataset_name}.yaml"
+            else:
+                data = f"{dataset_name}_{degradation}.yaml"
+            data = dataset_root / data
+
+            for split in config["splits"]:
+                dataset = Dataset(data, split)
+                yield path, dict(dataset=dataset)
 
 
 def parse_args():
@@ -86,13 +124,15 @@ if __name__ == "__main__":
     devices: list[int] = cfg.pop("devices")
     assert len(devices) > 0
 
-    # TODO: manage GPU queue?
-    num_devices = len(devices)
-    with thread.ThreadPoolExecutor(max_workers=num_devices) as executor:
+    device_pool = queue.Queue()
+    for device in devices:
+        device_pool.put(device)
+    with thread.ThreadPoolExecutor(max_workers=len(devices)) as executor:
         futures = []
         for i, (path, kwargs) in enumerate(parse_configs(cfg)):
-            device = devices[i % num_devices]
-            kwargs = dict(model=model.as_posix(), device=device, **kwargs, **cfg)
-            futures.append(executor.submit(run, config=path, name=name, **kwargs))
+            kwargs = dict(
+                config=path, name=name, model=model.as_posix(), **kwargs, **cfg
+            )
+            futures.append(executor.submit(run_with_device, device_pool, **kwargs))
         for f in as_completed(futures):
             f.result()
