798d2c5e4af404ab11a4b3afeb0f51b511380655
Leaf Ying <leaf.ying.work@gmail.com>
2026-05-27 21:43:21 +0800

    chore(dino): Remove useless argument in show_statistics.sh

--- projects/dino/tools/show_statistics.sh
+++ projects/dino/tools/show_statistics.sh
@@ -4,9 +4,9 @@ set -eu
 CWD=$(dirname "$0")
 
 CONFIG=$1
-CHECKPOINT=$2
+# CHECKPOINT=$2
 
 python "$CWD/base/analyze_model.py" \
     --config-file "$CONFIG" \
     --tasks flop parameter --num-inputs 100 \
-    train.init_checkpoint="$CHECKPOINT"
+#     train.init_checkpoint="$CHECKPOINT"
