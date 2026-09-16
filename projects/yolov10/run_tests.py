4661e2cf209656ec23b34bc8fd71f3410d126afa
Leaf Ying <leaf.ying.work@gmail.com>
2026-09-16 21:25:03 +0800

    feat(yolov10): Support evaluation per degradation in a single dataset

--- projects/yolov10/run_tests.py
+++ projects/yolov10/run_tests.py
@@ -2,6 +2,7 @@ import argparse
 import concurrent.futures.thread as thread
 import os
 import queue
+import shutil
 import subprocess
 import sys
 import tempfile
@@ -17,14 +18,33 @@ CONSOLE = Console()
 
 
 @dataclass
+class Subset:
+    name: str
+    split: str
+    robust: bool = False
+    split_subset: bool = False
+    degradation: Optional[str] = None
+
+
+@dataclass
 class Dataset:
     path: Path
-    split: str
+    subset: Subset
+
+
+def save_cfg(path: Path, cfg: dict):
+    with open(path, "w") as f:
+        yaml.safe_dump(cfg, f, sort_keys=False, allow_unicode=True)
+    return path
 
 
 def run(config: Path, name: str, dataset: Dataset, **kwargs):
+    tmp_dir = Path(tempfile.mkdtemp(prefix=f"{config.stem}_"))
+
     splits = [
-        item for item in dataset.split.replace("val", "").split("_") if len(item) != 0
+        item
+        for item in dataset.subset.name.replace("val", "").split("_")
+        if len(item) != 0
     ]
     splits = list(reversed(splits))
     if len(splits) != 0:
@@ -34,34 +54,38 @@ def run(config: Path, name: str, dataset: Dataset, **kwargs):
     with open(config, "r", encoding="utf-8") as f:
         cfg: dict = yaml.safe_load(f)
 
+    if dataset.subset.robust and dataset.subset.split_subset:
+        assert (
+            dataset.subset.degradation is not None
+            and dataset.subset.degradation != "degraded"
+        )
+        with open(dataset.path, "r", encoding="utf-8") as f:
+            dataset_cfg: dict = yaml.safe_load(f)
+
+        dataset_cfg["degradations"] = [dataset.subset.degradation]
+
+        dataset.path = tmp_dir / dataset.path.name
+        save_cfg(dataset.path, dataset_cfg)
+
     work_dir = Path(cfg["name"])
     # concat with folder of training dataset
     # test structure: <training dataset>/<testing dataset>/<exp_name>
     work_dir = work_dir.parent.parent / dataset.path.stem / name
     cfg["name"] = work_dir.as_posix()
     cfg["data"] = dataset.path.as_posix()
-    cfg["split"] = dataset.split
+    cfg["split"] = dataset.subset.split
     cfg.update(**kwargs)
 
-    with tempfile.NamedTemporaryFile(
-        prefix=f"{config.stem}_",
-        suffix=".yaml",
-        mode="w",
-        encoding="utf-8",
-        delete=False,
-    ) as file:
-        yaml.safe_dump(cfg, file, sort_keys=False, allow_unicode=True)
-
-    # with open(config, "w", encoding="utf-8") as f:
-    #     yaml.safe_dump(cfg, f, sort_keys=False, allow_unicode=True)
+    cfg_file = tmp_dir / config.name
+    save_cfg(cfg_file, cfg)
 
     # Command
-    cmd = [sys.executable, "run.py", file.name]
+    cmd = [sys.executable, "run.py", cfg_file.as_posix()]
     CONSOLE.print("Run command:", " ".join(cmd))
     try:
         subprocess.run(cmd, check=True)
     finally:
-        os.unlink(file.name)
+        shutil.rmtree(tmp_dir)
 
 
 def run_with_device(device_pool: queue.Queue, **kwargs):
@@ -79,6 +103,7 @@ def parse_configs(cfg: dict):
     dataset_root = Path(cfg.pop("dataset_root"))
     prefix: str = cfg.pop("prefix")
     degradations: Optional[list[str]] = cfg.pop("degradations", None)
+    split_subset: bool = cfg.pop("split_subset", False)
     configs: list[dict] = cfg.pop("configs")
 
     if degradations is None:
@@ -88,16 +113,37 @@ def parse_configs(cfg: dict):
         dataset_name = config["name"]
         path = root / f"{prefix}_{dataset_name}.yaml"
 
-        for degradation in degradations:
-            if degradation == "degraded":
-                data = f"{dataset_name}.yaml"
-            else:
-                data = f"{dataset_name}_{degradation}.yaml"
-            data = dataset_root / data
+        for split in config["splits"]:
+            if "degraded" in split:
+                for degradation in degradations:
+                    if not split_subset and degradation != "degraded":
+                        data = f"{dataset_name}_{degradation}.yaml"
+                    else:
+                        data = f"{dataset_name}.yaml"
+                    data = dataset_root / data
 
-            for split in config["splits"]:
-                dataset = Dataset(data, split)
-                yield path, dict(dataset=dataset)
+                    subset = Subset(
+                        split,
+                        split,
+                        robust="degraded" in split,
+                        split_subset=split_subset,
+                        degradation=degradation,
+                    )
+
+                    if (
+                        subset.robust
+                        and subset.degradation != "degraded"
+                        and subset.split_subset
+                    ):
+                        subset.name = subset.name.replace(
+                            "degraded",
+                            "_".join(reversed(degradation.split("_"))) + "_degraded",
+                        )
+                    yield path, dict(dataset=Dataset(data, subset))
+            else:
+                data = dataset_root / f"{dataset_name}.yaml"
+                subset = Subset(split, split)
+                yield path, dict(dataset=Dataset(data, subset))
 
 
 def parse_args():
